82e5f738a371530bcd145d4fb91743a2be1bd5c7
davidregui <davidreguilon@gmail.com>
2021-11-08 09:42:54 +0100

    Update instalacion_completa.sh

--- instalacion_completa.sh
+++ instalacion_completa.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-#clonamos el repositorio con los scripts
-sudo apt install git
-git clone https://github.com/davidregui/RPI_UBUNTU64_TF_OPCV_ROSNOETIC.git
-#nos movemos al repositorio
-cd RPI_UBUNTU64_TF_OPCV_ROSNOETIC/
 sh escritorio.sh
 sh ROS_noetic.sh
 sh opencv_forma1.sh
